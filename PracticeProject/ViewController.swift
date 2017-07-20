dd9951bd09f4a53cc95c059b2a4516db4167f11b
paggieli <paggieisme@gmail.com>
2017-07-20 19:09:06 +0800

    Initial Commit

---
+++ PracticeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeProject
+//
+//  Created by Admin on 2017/7/20.
+//  Copyright © 2017年 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
